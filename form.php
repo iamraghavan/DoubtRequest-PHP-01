de5fc70141cdc11abce9cf8b85d0d34c8cb54a05
DeveloperRaghavan <developer.raghavan@gmail.com>
2022-11-21 16:21:59 +0530

    first commit

---
+++ form.php
@@ -0,0 +1,94 @@
+<html>
+
+<head>
+
+    <title>Image Upload in PHP</title>
+
+    <! link the css file to style the form>
+
+        <link rel="stylesheet" type="text/css" href="style.css" />
+
+</head>
+
+<body>
+
+    <div id="wrapper">
+
+        <! specify the encoding type of the form using the enctype attribute>
+
+            <form method="POST" action="" enctype="multipart/form-data">
+
+                <input type="file" name="choosefile" value="" />
+                <input type="text" name="name" value="" />
+
+                <div>
+
+                    <button type="submit" name="uploadfile">
+
+                        UPLOAD
+
+                    </button>
+
+                </div>
+
+            </form>
+
+    </div>
+
+</body>
+
+</html>
+
+<?php
+
+error_reporting(0);
+
+?>
+
+<?php
+
+$msg = ""; 
+
+// check if the user has clicked the button "UPLOAD" 
+
+if (isset($_POST['uploadfile'])) {
+
+    $filename = $_FILES["choosefile"]["name"];
+    $name = $_POST['name'];
+
+    $tempname = $_FILES["choosefile"]["tmp_name"];  
+
+        $folder = "image/".$filename;   
+
+    // connect with the database
+
+    $db = mysqli_connect("localhost", "root", "", "testing"); 
+
+        // query to insert the submitted data
+
+        $sql = "INSERT INTO banner (banner_image, banner_title) VALUES ('$filename', $name)";
+
+        // function to execute above query
+
+        mysqli_query($db, $sql);       
+
+        // Add the image to the "image" folder"
+
+        if (move_uploaded_file($tempname, $folder)) {
+
+            $msg = "Image uploaded successfully";
+            echo "
+        <script type= 'text/javascript'>
+            alert('New record created successfully');
+        </script>";
+        }else{
+
+            $msg = "Failed to upload image";
+
+    }
+
+}
+
+$result = mysqli_query($db, "SELECT * FROM banner");
+
+?>
